fc1e19d124b8a4d183256e905cfa50ef10939ddd
simontuo <simontuo666@gmail.com>
2020-04-03 22:06:33 +0800

    新增客户详情页

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,9 +1,10 @@
 const getters = {
-	sidebar: state => state.app.sidebar,
-	device: state => state.app.device,
-	token: state => state.user.token,
-	avatar: state => state.user.avatar,
-	name: state => state.user.name,
-	table: state => state.app.table,
+    sidebar: state => state.app.sidebar,
+    device: state => state.app.device,
+    token: state => state.user.token,
+    avatar: state => state.user.avatar,
+    name: state => state.user.name,
+    table: state => state.app.table,
+    roles: state => state.user.roles,
 }
 export default getters
